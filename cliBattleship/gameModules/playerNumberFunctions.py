fd7aa4b65a5a279a4526f63c475f17d6656cf27d
Tyson Van Patten <tyson@descriptdata.com>
2021-07-24 17:39:34 -0400

    updated documentation

--- cliBattleship/gameModules/playerNumberFunctions.py
+++ cliBattleship/gameModules/playerNumberFunctions.py
@@ -4,6 +4,11 @@ from .playerPlays import playerShot
 
 
 def printTurnResult(turnResult):
+    """Prints the results of the turn, only used in 1 or 2 person games.
+
+    Args:
+        turnResult (DICT): Takes a dictionary of the turn result in json format
+    """
     if turnResult.get('shotResult') == 'M':
         print(f'{turnResult.get("offPlayer")} fired at '
               f'{turnResult.get("shot")} and missed.')
@@ -20,6 +25,17 @@ def printTurnResult(turnResult):
 
 
 def ComputerGameFunction(player1, player2):
+    """This is a single game function for computer only play.  The player
+    class for each player must be initialized before this can be called.
+    Once called it will play the entire game and return a game log.
+
+    Args:
+        player1 (Class): The Player 1 class
+        player2 (Class): The Player 2 class
+
+    Returns:
+        DICT: The full game log in a diction / json format
+    """
     gameLog = []
     players = randomPlayer(player1, player2)
     offPlayer = players[0]
@@ -47,6 +63,15 @@ def ComputerGameFunction(player1, player2):
 
 
 def OnePlayerGameFunction(player1, player2):
+    """This is a single game function for 1 person play.  The player
+    class for each player must be initialized before this can be called.
+    Once called it will play the entire game and print out a turn results
+    after each turn.
+
+    Args:
+        player1 (Class): The Player 1 class
+        player2 (Class): The Player 2 class
+    """
     gameLog = []
     players = randomPlayer(player1, player2)
     offPlayer = players[0]
