2fc8c0884cbbac9f988bf494f2bb8f2ae9606781
Tyson Van Patten <tyson@descriptdata.com>
2021-07-24 16:10:26 -0400

    added documentation

--- cliBattleship/gameModules/computerPlays.py
+++ cliBattleship/gameModules/computerPlays.py
@@ -2,7 +2,15 @@ import random
 
 
 def computerShot(offPlayer):
+    """Based upon player computer setting calls the correct computer shot
+    selection function.
 
+    Args:
+        offPlayer (Class): The Offensive Player
+
+    Returns:
+        STR: The location of the computer shot in string format, example A:5
+    """
     if offPlayer.computerLevel == 0:
         # Random shot
         shot = randomComputerShot(offPlayer)
@@ -24,7 +32,15 @@ def computerShot(offPlayer):
 
 
 def randomComputerShot(offPlayer):
+    """Selects a random location to shoot at, if location has previously been
+    selected this function will select another random location.
 
+    Args:
+        offPlayer (Class): The offensive player class
+
+    Returns:
+        STR: The location of the computer shot in string format, example A:5
+    """
     while True:
         pshot = random.choice(list(offPlayer.huntBoard.keys()))
         if offPlayer.huntBoard[pshot] != '0':
@@ -34,6 +50,17 @@ def randomComputerShot(offPlayer):
 
 
 def randomComputerShotHuntMode(offPlayer):
+    """Checks if the computer player is in Hunt mode (last shot was a hit
+    or there has been a hit but no ship sunk yet).  If player is in hunt mode
+    will call the huntShotReturn() function, if not will call the
+    randomComputerShot function.
+
+    Args:
+        offPlayer (Class): The offensive player class
+
+    Returns:
+        STR: The location of the computer shot in string format, example A:5
+    """
     if offPlayer.huntMode is True:
         return(huntShotReturn(offPlayer))
     else:
@@ -42,6 +69,17 @@ def randomComputerShotHuntMode(offPlayer):
 
 
 def parityComputerShotHuntMode(offPlayer):
+    """Checks if the computer player is in Hunt mode (last shot was a hit
+    or there has been a hit but no ship sunk yet).  If player is in hunt mode
+    will call the huntShotReturn() function, if not will call the
+    paritySearch function.
+
+    Args:
+        offPlayer (Class): The offensive player class
+
+    Returns:
+        STR: The location of the computer shot in string format, example A:5
+    """
     if offPlayer.huntMode is True:
         return(huntShotReturn(offPlayer))
     else:
@@ -50,6 +88,17 @@ def parityComputerShotHuntMode(offPlayer):
 
 
 def ProbDensityComputerShotHuntMode(offPlayer):
+    """Checks if the computer player is in Hunt mode (last shot was a hit
+    or there has been a hit but no ship sunk yet).  If player is in hunt mode
+    will call the huntShotReturn() function, if not will call the
+    probDensityComputerShot function.
+
+    Args:
+        offPlayer (Class): The offensive player class
+
+    Returns:
+        STR: The location of the computer shot in string format, example A:5
+    """
     if offPlayer.huntMode is True:
         return(huntShotReturn(offPlayer))
     else:
@@ -58,6 +107,16 @@ def ProbDensityComputerShotHuntMode(offPlayer):
 
 
 def paritySearch(offPlayer):
+    """The parity search uses the checkboard method to search for enemy ships
+    if no remaing shot are in the parity search space, the function will
+    select a random location that has not been shot at.
+
+    Args:
+        offPlayer (Class): Offensive Player
+
+    Returns:
+        STR: The location of the computer shot in string format, example A:5
+    """
     keyList = []
     xAxis = [i for i in range(1, 11)]
     yAxis = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
@@ -81,6 +140,19 @@ def paritySearch(offPlayer):
 
 
 def probDensityComputerShot(offPlayer):
+    """Calls the player cumlativeProbabiltiy method to calculate current
+    possibilities, will choose the highest probability.  The highest
+    probability will be checked to ensure the shot is not a previous hit.  If
+    shot is not a previous hit, function will return the shot, if a previous
+    hit function will choose the next highest possibility
+
+
+    Args:
+        offPlayer (Class): Offensive Player
+
+    Returns:
+        STR: The location of the computer shot in string format, example A:5
+    """
     while True:
         tempSort = sorted(offPlayer.cumlativeProb.items(),
                           key=lambda x: x[1], reverse=True)
@@ -93,7 +165,21 @@ def probDensityComputerShot(offPlayer):
                     return i[0]
 
 
+# Need to refactor this function to make it cleaner
 def huntShotReturn(offPlayer):
+    """When Offensive Player is in Hunt Mode, this function create a list for
+    the north possible locations, east, south, and west.  To narrow the search
+    area for the next shot.  After all directional lists are returned the
+    function will go to the north list if the next available location is a M
+    the function will move to the east list and follow the same pattern.  If
+    the north list contains a blank or H, the function will fire at that shot.
+
+    Args:
+        offPlayer (Class): The Offensive Player
+
+    Returns:
+        STR: The location of the computer shot in string format, example A:5
+    """
     xAxis = [i for i in range(1, 11)]
     yAxis = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
     pos = offPlayer.huntModeStartShot.split(':')
@@ -203,6 +289,17 @@ def huntShotReturn(offPlayer):
 
 
 def getOffPlayerHuntboardkey(val, offPlayer):
+    """This function will cycle through the Player huntboard looking for the
+    next unused location and return a list of unused locations
+
+    Args:
+        val (STR): The location of the computer shot in string
+        format, example A:5
+        offPlayer (Class): The offensive player
+
+    Returns:
+        STR: The location of the computer shot in string format, example A:5
+    """
     tempList = []
     for key, value in offPlayer.huntBoard.items():
         if val == value:
